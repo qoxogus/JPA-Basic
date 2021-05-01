5f3c184aa068325b8aca86d204d1c801a27b8c3d
qoxogus <a01034367055@gmail.com>
2021-05-01 13:24:56 +0900

    [기본키 매핑]

--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -11,25 +11,25 @@ import java.util.Date;
 @Setter
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String username;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) //Enum타입이 ordinal(defualt) -> enum순서(숫자)를 데이터베이스에 저장 / string -> enum이름을 데이터베이스에 저장
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob //varchar를 넘어서는 큰 컨텐츠가 들어가야 할 컬럼
-    private String description;
+//    private Integer age;
+//
+//    @Enumerated(EnumType.STRING) //Enum타입이 ordinal(defualt) -> enum순서(숫자)를 데이터베이스에 저장 / string -> enum이름을 데이터베이스에 저장
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    @Lob //varchar를 넘어서는 큰 컨텐츠가 들어가야 할 컬럼
+//    private String description;
 
     public Member(){
     }
