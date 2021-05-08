53daaa4f6eea1fb28aea51fc54f06e8752b8ca09
qoxogus <a01034367055@gmail.com>
2021-05-08 17:04:43 +0900

    [ADD Comment]

--- src/main/java/hellojpa/domain/BaseEntity.java
+++ src/main/java/hellojpa/domain/BaseEntity.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@MappedSuperclass
+@MappedSuperclass //공동매핑 정보가 필요할 때 사용한다     이 클래스를 다른 엔티티에 상속 시키면 테이블에 이 컬럼들이 추가된다
 public abstract class BaseEntity {
 
     private String createBy;
