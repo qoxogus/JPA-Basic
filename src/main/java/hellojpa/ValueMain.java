27cc3cbaf84d5f276f2a9de04574b795be378fb4
qoxogus <a01034367055@gmail.com>
2021-05-10 14:48:16 +0900

    [기본값 타입]

---
+++ src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,23 @@
+package hellojpa;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = a; //a의 값이 복사되어 b에 들어감
+//
+//        a = 20;
+//
+//        System.out.println("a = " + a); //20
+//        System.out.println("b = " + b); //10
+
+        Integer a = new Integer(10);
+        Integer b = a;
+
+        a.setValue(20); //만약 이렇게 값을 변경하게 된다면 (같은 인스턴스 공유이기 때문에) println에서 20, 20이 출력되게된다
+
+        System.out.println("a = " + a); //10
+        System.out.println("b = " + b); //10
+    }
+}
