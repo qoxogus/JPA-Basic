2554f5f07d47de356229546f214f7648f00bfe06
qoxogus <a01034367055@gmail.com>
2021-05-13 10:18:04 +0900

    [실전예제 6 - 값 타입 매핑]

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -12,12 +12,12 @@ public class ValueMain {
 //        System.out.println("a = " + a); //20
 //        System.out.println("b = " + b); //10
 
-        Integer a = new Integer(10);
-        Integer b = a;
-
-        a.setValue(20); //만약 이렇게 값을 변경하게 된다면 (같은 인스턴스 공유이기 때문에) println에서 20, 20이 출력되게된다
-
-        System.out.println("a = " + a); //10
-        System.out.println("b = " + b); //10
+//        Integer a = new Integer(10);
+//        Integer b = a;
+//
+////        a.setValue(20); //만약 이렇게 값을 변경하게 된다면 (같은 인스턴스 공유이기 때문에) println에서 20, 20이 출력되게된다
+//
+//        System.out.println("a = " + a); //10
+//        System.out.println("b = " + b); //10
     }
 }
